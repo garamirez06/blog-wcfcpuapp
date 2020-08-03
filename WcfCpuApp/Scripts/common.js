685feb3877bb1a5854f613a32baaf68e998f6e40
Gustavo Ramirez <garamirez06@hotmail.com>
2020-08-03 07:40:18 -0300

    cambios en front

---
+++ WcfCpuApp/Scripts/common.js
@@ -0,0 +1,8 @@
+﻿function copiarAlPortapapeles(id_elemento) {
+    var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById(id_elemento).innerText);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+}
